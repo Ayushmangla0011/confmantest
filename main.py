4528ea3ec0204ecd64ddbc2e5a89b3e24e13ed6f
Ayushmangla0011 <54243227+Ayushmangla0011@users.noreply.github.com>
2023-01-17 00:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,35 @@ def extractor():
     data = request.json
     abs1 =data["abstract"]
     keys =  requests.post("https://ankitgaur2811-confman-key.hf.space/run/predict", json={"data": [abs1]}).json()
-    datak = keys["data"]
-    data1=datak[0].split("[")[1]
-    data1 = data1.split("]")[0]
-    data1 =data1.split(",")
-    datak = []
-    for x in data1:
-        datak.append(x.split("'")[1])
-    finalkey = datak + data["tags"]
+    datak = keys["data"][0]
+    text1 = datak.split("[")[1].split("]")[0]
+    text2 = text1.split(",")
+    i=[]
+    for x in text2:
+        i.append(x.split("'")[1])
+    finalkey = i + data["tags"]
     allreviewer = requests.get("https://confman-api.onrender.com/reviewers/all")
     allreviewer1 = allreviewer.json()
     allreviewer2 = jsonify(allreviewer1)
-    allrev=allreviewer2["result"]
-    reviewer = requests.post("http://ankitgaur2811.pythonanywhere.com/assignreviewer",data = {"allreviewer":allrev,"allpaperkeywords":finalkey,"assignedreview":[]})
-    return reviewer
+    allrev = allreviewer2.json
+    request1 = {"allreviewer":allrev["result"],"allpaperkeywords":finalkey,"assignedreview":[]}
+    print(request1)
+    payload = json.dumps(request1)
+    url = "http://ankitgaur2811.pythonanywhere.com/assignreviewer"
+    headers = {
+  'Content-Type': 'application/json'
+}
+    reviewer = requests.request("POST", url, headers=headers, data=payload)
+    # print("=========================================================")
+    # if reviewer.status_code != 200:
+    #     print(reviewer.status_code)
+    # else:
+    #     try:
+    #         data = reviewer.json()
+    #         print(data)
+    #     except ValueError:
+    #         print(reviewer.text)
+    return reviewer.text
 
 
 if __name__ == "__main__":
